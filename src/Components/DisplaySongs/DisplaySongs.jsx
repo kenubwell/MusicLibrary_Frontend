424abc14c950be79d169628671319ad2c9873564
unknown <kenblackuoo@gmail.com>
2022-03-14 18:07:06 -0400

    Updating styling for DisplaySongs component

--- src/Components/DisplaySongs/DisplaySongs.jsx
+++ src/Components/DisplaySongs/DisplaySongs.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from 'react';
 import './DisplaySongs.css';
 
 const DisplaySongs = (props) => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (  
-    <div>  
-        <table className='song-table'>
+    <div className = 'display-container'>
+        <div className='table-title'>
+            <div>
+                <h2 className='library-title'>Song Library</h2>
+            </div>
+            <div className = 'search-filter'>
+                <label>Filter Library</label>
+                <input type='text' placeholder="Search...(e.g. Pop music)" onChange={(event) => setSearchTerm(event.target.value)}/>
+            </div>
+        </div>
+        <table className='song-table table'>
         <thead>
-            <tr>
+            <tr className = 'header-row'>
                 <th>Title</th>
                 <th>Artist</th>
                 <th>Album</th>            
@@ -15,9 +27,17 @@ const DisplaySongs = (props) => {
             </tr>
         </thead>
         <tbody>
-            {props.displaySongs.map((song, index) => { 
+            {props.displaySongs.filter((song) =>{
+                if (searchTerm == ""){
+                    return song;
+                }
+                else if (song.title.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || song.artist.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || song.album.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || song.release_date.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || song.genre.toLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                    return song;
+                }
+            })
+            .map((song, index) => { 
             return (
-                <tr key= {index}>
+                <tr key= {index} className = 'display-rows'>
                     <td>{song.title}</td>
                     <td>{song.artist}</td>
                     <td>{song.album}</td>
