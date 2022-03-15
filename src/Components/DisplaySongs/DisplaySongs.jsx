6c10bf40d34889b4315a25a9772a21ccc3ab3244
unknown <kenblackuoo@gmail.com>
2022-03-15 11:31:07 -0400

    Updated css styling on the Delete button/function

--- src/Components/DisplaySongs/DisplaySongs.jsx
+++ src/Components/DisplaySongs/DisplaySongs.jsx
@@ -44,7 +44,7 @@ const DisplaySongs = (props) => {
                     <td>{song.album}</td>
                     <td>{song.release_date}</td>
                     <td>{song.genre}</td>
-                    <button className ='delete-button' onClick={() => props.deleteSongProp(song)}>Delete</button>
+                    <td><button type = 'submit' className ='delete-button' onClick={() => props.deleteSongProp(song)}>Delete</button></td>
                 </tr>
             )
             })}
